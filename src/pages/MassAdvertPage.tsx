89fe41b5a787e579d7adc3652b201334dd0e5a23
Ledatu <zavoronok.danila@gmail.com>
2024-01-28 00:59:21 +0300

    changed default daterange

--- src/pages/MassAdvertPage.tsx
+++ src/pages/MassAdvertPage.tsx
@@ -654,9 +654,9 @@ export const MassAdvertPage = () => {
             .replace(/(\d{2})\.(\d{2})\.(\d{4})/, '$3-$2-$1')
             .slice(0, 10),
     );
-    const monthAgo = new Date(today);
-    monthAgo.setDate(monthAgo.getDate() - 30);
-    const [dateRange, setDateRange] = useState([monthAgo, today]);
+    // const monthAgo = new Date(today);
+    // monthAgo.setDate(monthAgo.getDate() - 30);
+    const [dateRange, setDateRange] = useState([today, today]);
     const [startDate, endDate] = dateRange;
     const fieldRef = useRef(null);
     const [datePickerOpen, setDatePickerOpen] = useState(false);
