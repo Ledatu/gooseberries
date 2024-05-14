0a6de8edfc5d1bbfab2f71f3e1c94a86ce780e3d
Ledatu <zavoronok.danila@gmail.com>
2024-05-14 20:16:12 +0300

    analytics freee uze

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -71,10 +71,6 @@ export const Dashboard = () => {
         {
             value: 'analytics',
             content: 'Аналитика',
-            disabled:
-                Userfront.user.userUuid !== '4a1f2828-9a1e-4bbf-8e07-208ba676a806' &&
-                Userfront.user.userUuid !== '674281bf-7a48-463a-825d-a5c4163b4819' &&
-                Userfront.user.userUuid !== '46431a09-85c3-4703-8246-d1b5c9e52594',
         },
         // {
         //     value: 'deliveryOrders',
