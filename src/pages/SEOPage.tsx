4aa0635cbbaedfe99a459e75b1cdca3c6cfd0fc0
Ledatu <zavoronok.danila@gmail.com>
2024-07-23 02:04:51 +0300

    seo

--- src/pages/SEOPage.tsx
+++ src/pages/SEOPage.tsx
@@ -1,4 +1,4 @@
-import {Card, TextInput} from '@gravity-ui/uikit';
+import {Card, Loader, Pagination, TextInput} from '@gravity-ui/uikit';
 import React, {useEffect, useRef, useState} from 'react';
 import {motion} from 'framer-motion';
 import TheTable, {compare} from 'src/components/TheTable';
@@ -8,9 +8,15 @@ export const SEOPage = () => {
     const [isInputDown, setIsInputDown] = useState(true);
     const mainInputRef = useRef<HTMLInputElement>(null);
 
+    const pageSize = 300;
+
     const [dataPhrasesTable, setDataPhrasesTable] = useState({});
     const [filtersPhrasesTable, setFiltersPhrasesTable] = useState({undef: false});
     const [filteredDataPhrasesTable, setFilteredDataPhrasesTable] = useState([] as any[]);
+    const [filteredSummaryPhrasesTable, setFilteredSummaryPhrasesTable] = useState({});
+    const [paginatedDataPhrasesTable, setPaginatedDataPhrasesTable] = useState([] as any[]);
+    const [pagesTotalPhrasesTable, setPagesTotalPhrasesTable] = useState(1);
+    const [currentPagePhrasesTable, setCurrentPagePhrasesTable] = useState(1);
     const columnsPhrasesTable = [
         {name: 'phrase', placeholder: 'Поисковая фраза', valueType: 'text'},
         {name: 'freq', placeholder: 'Частота'},
@@ -19,10 +25,17 @@ export const SEOPage = () => {
     const [dataWordsTable, setDataWordsTable] = useState({});
     const [filtersWordsTable, setFiltersWordsTable] = useState({undef: false});
     const [filteredDataWordsTable, setFilteredDataWordsTable] = useState([] as any[]);
+    const [filteredSummaryWordsTable, setFilteredSummaryWordsTable] = useState({});
+    const [paginatedDataWordsTable, setPaginatedDataWordsTable] = useState([] as any[]);
+    const [pagesTotalWordsTable, setPagesTotalWordsTable] = useState(1);
+    const [currentPageWordsTable, setCurrentPageWordsTable] = useState(1);
     const columnsWordsTable = [
         {name: 'word', placeholder: 'Слово', valueType: 'text'},
         {name: 'freq', placeholder: 'Частота'},
     ];
+
+    const [fetchingPhrases, setFetchingPhrases] = useState(true);
+    const [lastFetchedPhrase, setLastFetchedPhrase] = useState('');
 
     const handleMainInputKeyDown = (event) => {
         if (event.key === 'Enter') {
@@ -33,19 +46,25 @@ export const SEOPage = () => {
 
                 const params = {
                     uid: getUid(),
-                    phraseFilter: val,
+                    filterPhrase: val,
+                    escapeChars: true,
                 };
                 console.log(params);
 
-                callApi('getRequestsFiltered', params)
-                    .then((res) => {
-                        if (!res) return;
-                        const data = res['data'];
-                        setDataPhrasesTable(data);
-                    })
-                    .catch((error) => {
-                        console.log(new Date(), 'error occured:', error);
-                    });
+                if (isValid && lastFetchedPhrase != val) {
+                    setFetchingPhrases(true);
+                    setLastFetchedPhrase(val);
+                    callApi('getRequestsFiltered', params)
+                        .then((res) => {
+                            if (!res) return;
+                            const data = res['data'];
+                            setDataPhrasesTable(data);
+                        })
+                        .catch((error) => {
+                            console.log(new Date(), 'error occured:', error);
+                        })
+                        .finally(() => setFetchingPhrases(false));
+                }
             }
         }
     };
@@ -102,13 +121,28 @@ export const SEOPage = () => {
     };
 
     const filterDataWordsTable = (withfFilters = {}, tableData = {}) => {
-        filterDataTable(
+        const temp = filterDataTable(
             withfFilters,
             tableData,
             filtersWordsTable,
             dataWordsTable,
             setFilteredDataWordsTable,
         );
+
+        const paginatedDataTemp = temp.slice(0, 300);
+        const filteredSummaryTemp = {
+            word: `На странице: ${paginatedDataTemp.length} Всего: ${temp.length}`,
+            freq: 0,
+        };
+
+        for (const row of temp) {
+            const {freq} = row;
+            filteredSummaryTemp.freq += freq;
+        }
+        setFilteredSummaryWordsTable(filteredSummaryTemp);
+        setPaginatedDataWordsTable(paginatedDataTemp);
+        setCurrentPageWordsTable(1);
+        setPagesTotalWordsTable(temp.length);
     };
     useEffect(() => {
         filterDataWordsTable(filtersWordsTable, dataWordsTable);
@@ -123,15 +157,25 @@ export const SEOPage = () => {
             setFilteredDataPhrasesTable,
         );
 
+        const paginatedDataTemp = temp.slice(0, 300);
+        const filteredSummaryTemp = {
+            phrase: `На странице: ${paginatedDataTemp.length} Всего: ${temp.length}`,
+            freq: 0,
+        };
+
         const tempDataWordsTable = {};
         for (const row of temp) {
             const {phrase, freq} = row;
-
+            filteredSummaryTemp.freq += freq;
             for (const word of phrase.split(' ')) {
                 if (!tempDataWordsTable[word]) tempDataWordsTable[word] = {word, freq: 0};
                 tempDataWordsTable[word].freq += freq;
             }
         }
+        setFilteredSummaryPhrasesTable(filteredSummaryTemp);
+        setPaginatedDataPhrasesTable(paginatedDataTemp);
+        setCurrentPagePhrasesTable(1);
+        setPagesTotalPhrasesTable(temp.length);
 
         setDataWordsTable(tempDataWordsTable);
     };
@@ -141,9 +185,9 @@ export const SEOPage = () => {
 
     const tableCardStyle = {
         width: '100%',
-        // maxHeight: 'calc(100vh - 10em - 68px - 32px - 36px - 48px - 30px)',
-        boxShadow: 'inset 0px 0px 10px var(--g-color-base-background)',
+        boxShadow: 'var(--g-color-base-background) 0px 2px 8px',
         overflow: 'auto',
+        borderRadius: 9,
     };
 
     return (
@@ -156,7 +200,7 @@ export const SEOPage = () => {
             }}
         >
             <motion.div
-                animate={{marginTop: isInputDown ? 300 : 0}}
+                animate={{marginTop: isInputDown ? 300 : 0, width: isInputDown ? '50vw' : '45vw'}}
                 transition={{
                     type: 'spring',
                     damping: 100,
@@ -177,7 +221,24 @@ export const SEOPage = () => {
                 />
             </motion.div>
             <motion.div
-                animate={{opacity: isInputDown ? 0 : 1}}
+                animate={{opacity: fetchingPhrases && !isInputDown ? 1 : 0}}
+                transition={{duration: 0.1}}
+                style={{
+                    opacity: 0,
+                    pointerEvents: 'none',
+                    top: 76,
+                    position: 'absolute',
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    width: '100vw',
+                    height: 208,
+                }}
+            >
+                <Loader size="l" />
+            </motion.div>
+            <motion.div
+                animate={{opacity: isInputDown ? 0 : 1, top: fetchingPhrases ? 300 : 74}}
                 transition={{
                     type: 'spring',
                     damping: 100,
@@ -191,7 +252,7 @@ export const SEOPage = () => {
                     flexDirection: 'row',
                     justifyContent: 'center',
                     width: '100vw',
-                    padding: '0 40px',
+                    // padding: '0 40px',
                     opacity: 0,
                 }}
             >
@@ -206,6 +267,9 @@ export const SEOPage = () => {
                     animate={{
                         x: isInputDown ? -1000 : 0,
                         opacity: !isInputDown ? 1 : 0,
+                        maxHeight: fetchingPhrases
+                            ? 'calc(100vh - 60px - 76px - 300px)'
+                            : 'calc(100vh - 60px - 76px - 40px)',
                     }}
                     transition={{
                         type: 'spring',
@@ -215,6 +279,7 @@ export const SEOPage = () => {
                     style={{
                         x: -1000,
                         maxWidth: '50vw',
+                        maxHeight: 'calc(100vh - 60px - 76px - 40px)',
                         opacity: 0,
                         display: 'flex',
                         flexDirection: 'column',
@@ -224,12 +289,35 @@ export const SEOPage = () => {
                     <Card style={tableCardStyle}>
                         <TheTable
                             columnData={columnsPhrasesTable}
-                            data={filteredDataPhrasesTable}
+                            data={paginatedDataPhrasesTable}
                             filters={filtersPhrasesTable}
                             setFilters={setFiltersPhrasesTable}
                             filterData={filterDataPhrasesTable}
+                            footerData={[filteredSummaryPhrasesTable]}
                         />
                     </Card>
+                    <div style={{minHeight: 8}} />
+                    <Pagination
+                        total={pagesTotalPhrasesTable}
+                        page={currentPagePhrasesTable}
+                        pageSize={pageSize}
+                        onUpdate={(page) => {
+                            setCurrentPagePhrasesTable(page);
+                            const paginatedDataTemp = filteredDataPhrasesTable.slice(
+                                (page - 1) * pageSize,
+                                page * pageSize,
+                            );
+                            setFilteredSummaryPhrasesTable((row) => {
+                                const temp = row;
+                                temp[
+                                    'phrase'
+                                ] = `На странице: ${paginatedDataTemp.length} Всего: ${filteredDataPhrasesTable.length}`;
+
+                                return temp;
+                            });
+                            setPaginatedDataPhrasesTable(paginatedDataTemp);
+                        }}
+                    />
                 </motion.div>
                 <div style={{minWidth: 32}} />
                 <motion.div
@@ -240,6 +328,9 @@ export const SEOPage = () => {
                     animate={{
                         x: isInputDown ? 1000 : 0,
                         opacity: !isInputDown ? 1 : 0,
+                        maxHeight: fetchingPhrases
+                            ? 'calc(100vh - 60px - 76px - 300px)'
+                            : 'calc(100vh - 60px - 76px - 40px)',
                     }}
                     transition={{
                         type: 'spring',
@@ -249,6 +340,7 @@ export const SEOPage = () => {
                     style={{
                         x: 1000,
                         maxWidth: '30vw',
+                        maxHeight: 'calc(100vh - 60px - 76px - 40px)',
                         opacity: 0,
                         display: 'flex',
                         flexDirection: 'column',
@@ -258,12 +350,35 @@ export const SEOPage = () => {
                     <Card style={tableCardStyle}>
                         <TheTable
                             columnData={columnsWordsTable}
-                            data={filteredDataWordsTable}
+                            data={paginatedDataWordsTable}
                             filters={filtersWordsTable}
                             setFilters={setFiltersWordsTable}
                             filterData={filterDataWordsTable}
+                            footerData={[filteredSummaryWordsTable]}
                         />
                     </Card>
+                    <div style={{minHeight: 8}} />
+                    <Pagination
+                        total={pagesTotalWordsTable}
+                        page={currentPageWordsTable}
+                        pageSize={pageSize}
+                        onUpdate={(page) => {
+                            setCurrentPageWordsTable(page);
+                            const paginatedDataTemp = filteredDataWordsTable.slice(
+                                (page - 1) * pageSize,
+                                page * pageSize,
+                            );
+                            setFilteredSummaryWordsTable((row) => {
+                                const temp = row;
+                                temp[
+                                    'word'
+                                ] = `На странице: ${paginatedDataTemp.length} Всего: ${filteredDataWordsTable.length}`;
+
+                                return temp;
+                            });
+                            setPaginatedDataWordsTable(paginatedDataTemp);
+                        }}
+                    />
                 </motion.div>
             </motion.div>
         </div>
