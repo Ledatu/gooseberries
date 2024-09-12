059063c225b1dfef225a4ebcc150ae2c073d7324
Ledatu <zavoronok.danila@gmail.com>
2024-09-12 06:30:50 +0300

    fix pretier

--- src/components/RequireAuth.tsx
+++ src/components/RequireAuth.tsx
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react';
 import {useLocation, Navigate} from 'react-router-dom';
-import axios from 'axios';
 import callApi from 'src/utilities/callApi';
 
 function RequireAuth({children}) {
