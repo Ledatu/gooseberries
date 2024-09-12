625b6bb830bdadcf82e72ce0645fbe2017e80165
Ledatu <zavoronok.danila@gmail.com>
2024-09-13 02:20:43 +0300

    govno

--- src/components/RequireAuth.tsx
+++ src/components/RequireAuth.tsx
@@ -30,7 +30,7 @@ function RequireAuth({children}) {
 
             if (response.data.valid) {
                 setIsAuthenticated(true); // Token is valid, set authenticated
-                setUserInfo(response.data.user); // Store user info
+                setUserInfo(response.data); // Store user info
             } else {
                 localStorage.removeItem('authToken'); // Remove invalid token
                 setIsAuthenticated(false); // Token is invalid, set not authenticated
