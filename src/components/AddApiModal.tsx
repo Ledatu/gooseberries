6802adf9d3e5df15d9a6c2855be419b56a4946c0
Ledatu <zavoronok.danila@gmail.com>
2024-12-10 12:50:57 +0300

    addapimodal

--- src/components/AddApiModal.tsx
+++ src/components/AddApiModal.tsx
@@ -1,10 +1,12 @@
 import {Button, Card, Modal, TextInput, Text, Link} from '@gravity-ui/uikit';
 import {motion} from 'framer-motion';
-import React, {Children, isValidElement, ReactElement, useState} from 'react';
+import React, {Children, isValidElement, ReactElement, useMemo, useState} from 'react';
 import {useUser} from './RequireAuth';
-import screen from '../assets/api-key.jpg';
+// import screen from '../assets/api-key.jpg';
 import {useError} from 'src/pages/ErrorContext';
 import ApiClient from 'src/utilities/ApiClient';
+import {PasteTextField} from './PasteTextField';
+import {TextTitleWrapper} from './TextTitleWrapper';
 
 interface AddApiModalInterface {
     children: ReactElement | ReactElement[];
@@ -19,11 +21,97 @@ export const AddApiModal = ({children}: AddApiModalInterface) => {
 
     const [name, setName] = useState('');
     const [key, setKey] = useState('');
+    const [readKey, setReadKey] = useState('');
+
+    const [sellerId, setSellerId] = useState('');
+
+    const [modules, setModules] = useState({
+        content: {name: 'Контент', presented: false, readOnly: false},
+        analytics: {name: 'Аналитика', presented: false, readOnly: false},
+        prices_and_discounts: {name: 'Цены и скидки', presented: false, readOnly: false},
+        marketplace: {name: 'Маркетплейс', presented: false, readOnly: false},
+        statistics: {name: 'Статистика', presented: false, readOnly: false},
+        promotion: {name: 'Продвижение', presented: false, readOnly: false},
+        questions_and_feedbacks: {name: 'Вопросы и отзывы', presented: false, readOnly: false},
+    });
+
+    const modulesValid = useMemo(() => {
+        for (const [_, data] of Object.entries(modules)) {
+            if (!data?.presented) return false;
+        }
+        return true;
+    }, [modules]);
+
+    const moduleButtons = useMemo(() => {
+        const buttons = [] as any[];
+        for (const [_, data] of Object.entries(modules)) {
+            buttons.push(
+                <Button
+                    size="l"
+                    view={
+                        data?.presented
+                            ? data?.readOnly
+                                ? 'outlined-warning'
+                                : 'outlined-success'
+                            : 'outlined'
+                    }
+                    selected={data?.presented}
+                    pin="circle-circle"
+                >
+                    {data?.name}
+                </Button>,
+            );
+        }
+        return buttons;
+    }, [modules]);
+
+    const checkApiToken = async (token, isOnlyRead) => {
+        try {
+            const response = await ApiClient.post('auth/decode-api-token', {token});
+            console.log('auth/decode-api-token', response);
+            if (!response) throw new Error('Не удалось обработать токен');
+
+            const {sid, modules: tokenModules} = response.data as any;
+
+            if (sellerId != '' && sid != sellerId)
+                throw new Error('Использован токен другого магазина');
+
+            setSellerId(sid);
+
+            const temp = {...modules};
+            const readOnly = tokenModules.includes('read-only');
+            if (isOnlyRead != readOnly) throw new Error('Вы вставили токен не в его поле');
+            for (const key of Object.keys(temp)) {
+                if (tokenModules.includes(key)) {
+                    temp[key].presented = true;
+                    temp[key].readOnly = readOnly;
+                }
+            }
+            console.log(temp, tokenModules);
+
+            setModules(temp);
+        } catch (error) {
+            console.error(error);
+            showError(
+                error?.response?.data?.error || error?.message || 'Не удалось обработать токен',
+            );
+        }
+    };
 
     const handleOpen = async () => {
+        setModules({
+            content: {name: 'Контент', presented: false, readOnly: false},
+            analytics: {name: 'Аналитика', presented: false, readOnly: false},
+            prices_and_discounts: {name: 'Цены и скидки', presented: false, readOnly: false},
+            marketplace: {name: 'Маркетплейс', presented: false, readOnly: false},
+            statistics: {name: 'Статистика', presented: false, readOnly: false},
+            promotion: {name: 'Продвижение', presented: false, readOnly: false},
+            questions_and_feedbacks: {name: 'Вопросы и отзывы', presented: false, readOnly: false},
+        });
         setOpen(true);
         setName('');
         setKey('');
+        setReadKey('');
     };
 
     const handleClose = () => {
@@ -82,39 +170,69 @@ export const AddApiModal = ({children}: AddApiModalInterface) => {
                             border: '1px solid #eee2',
                         }}
                     >
-                        <Card
+                        <div
                             style={{
-                                background: 'var(--g-color-base-background)',
-                                height: 300,
-                                width: 300,
+                                maxWidth: 350,
+                                display: 'flex',
+                                flexDirection: 'row',
+                                flexWrap: 'wrap',
+                                columnGap: 8,
+                                rowGap: 8,
+                            }}
+                        >
+                            {moduleButtons}
+                        </div>
+                        <div
+                            style={{
                                 display: 'flex',
                                 flexDirection: 'column',
+                                rowGap: 8,
+                                marginTop: 8,
                             }}
                         >
                             <div
                                 style={{
                                     display: 'flex',
-                                    flexDirection: 'column',
-                                    height: '100%',
-                                    borderRadius: 9,
-                                    overflow: 'hidden',
+                                    flexDirection: 'row',
+                                    alignItems: 'center',
+                                    columnGap: 4,
                                 }}
                             >
-                                <img src={screen} style={{height: '100%'}} />
+                                <Button size="s" pin="circle-circle" view="flat-success" selected>
+                                    Зеленый
+                                </Button>
+                                - Раздел доступен на чтение и запись
                             </div>
-                        </Card>
+                            <div
+                                style={{
+                                    display: 'flex',
+                                    flexDirection: 'row',
+                                    alignItems: 'center',
+                                    columnGap: 4,
+                                }}
+                            >
+                                <Button size="s" pin="circle-circle" view="flat-warning" selected>
+                                    Желтый
+                                </Button>
+                                - Раздел доступен только на чтение
+                            </div>
+                        </div>
                         <Text
-                            variant="caption-2"
+                            variant="body-1"
                             color="secondary"
                             style={{
+                                width: 300,
                                 margin: '8px',
                                 marginBottom: 30,
+                                textWrap: 'wrap',
                             }}
                         >
-                            Так должны выглядеть настройки API токена.
+                            API токен(-ы), вместе, должны включать все следующие разделы. Вы можете
+                            добавить часть из них только на чтение, но тогда будет доступен не весь
+                            функционал.
                         </Text>
                         <motion.div
-                            animate={{height: open ? 208 : 0}}
+                            animate={{height: open ? 300 : 0}}
                             style={{
                                 height: 0,
                                 width: 300,
@@ -132,23 +250,41 @@ export const AddApiModal = ({children}: AddApiModalInterface) => {
                                 size="l"
                                 placeholder="Введите название магазина"
                             />
-                            <TextInput
-                                value={key}
-                                onUpdate={(val) => setKey(val)}
-                                size="l"
-                                placeholder="Вставьте API токен"
-                            />
+                            <div style={{minHeight: 8}} />
+                            <TextTitleWrapper title={'Токен на чтение и запись'} padding={8}>
+                                <PasteTextField
+                                    onPaste={(value) => {
+                                        checkApiToken(value, false);
+                                        setKey(value);
+                                    }}
+                                />
+                            </TextTitleWrapper>
+                            <div style={{minHeight: 8}} />
+                            <TextTitleWrapper
+                                title={'Токен только на чтение (не обязательно)'}
+                                padding={8}
+                            >
+                                <PasteTextField
+                                    onPaste={(value) => {
+                                        checkApiToken(value, true);
+                                        setReadKey(value);
+                                    }}
+                                />
+                            </TextTitleWrapper>
+                            <div style={{minHeight: 8}} />
                             <Button
                                 width="max"
                                 size="l"
                                 view="outlined-success"
                                 selected
-                                disabled={name === '' || key === ''}
+                                disabled={name === '' || key === '' || !modulesValid}
                                 onClick={async () => {
                                     const params = {
                                         user_id: user?._id,
                                         campaignName: name,
                                         apiKey: key,
+                                        apiReadKey: readKey,
+                                        modules: modules,
                                     };
                                     try {
                                         await ApiClient.post('auth/create-campaign', params);
@@ -165,7 +301,7 @@ export const AddApiModal = ({children}: AddApiModalInterface) => {
                                 <Text variant="subheader-1">Добавить магазин</Text>
                             </Button>
                             <Text style={{margin: '0 16px'}} variant="caption-2" color="secondary">
-                                Вы можете начать работать уже через 30 минут, а через 3 часа вам
+                                Вы можете начать работать уже через 30 минут, а через 3 - 4 часа вам
                                 будет доступна подробная статистика за последние 90 дней!
                             </Text>
                             <Link
