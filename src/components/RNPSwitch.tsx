9a31f2ce3a540ef20bf7a8aa9a8c1e1a4ae6ca0a
VerdinKirill <kirill0verdin@yandex.ru>
2024-12-27 02:03:35 +0300

    RNPSwitch

--- src/components/RNPSwitch.tsx
+++ src/components/RNPSwitch.tsx
@@ -1,11 +1,12 @@
 import React, {useState, useEffect} from 'react';
 
-import {Switch, Tooltip, Text, Loader} from '@gravity-ui/uikit';
+import {Switch, Text, Loader} from '@gravity-ui/uikit';
 import {useError} from 'src/pages/ErrorContext';
 
 import ApiClient from 'src/utilities/ApiClient';
 import {useUser} from './RequireAuth';
 import {motion} from 'framer-motion';
+import {HelpPopover} from '@gravity-ui/components';
 
 export const RNPSwitch = () => {
     const {userInfo} = useUser();
@@ -45,36 +46,39 @@ export const RNPSwitch = () => {
     }, []);
 
     return (
-        <Tooltip content="Получать уведомления РНП">
-            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                <motion.div
-                    style={{
-                        display: 'flex',
-                        flexDirection: 'row',
-                        alignItems: 'center',
-                        opacity: 0,
+        // <Tooltip content="Получать уведомления РНП">
+        <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+            <motion.div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    opacity: 0,
+                }}
+                animate={{opacity: state === undefined ? 0 : 1}}
+                transition={{delay: 0.1}}
+            >
+                <Switch
+                    checked={state}
+                    onUpdate={(val) => {
+                        setNotification(val);
                     }}
-                    animate={{opacity: state === undefined ? 0 : 1}}
-                    transition={{delay: 0.1}}
-                >
-                    <Switch
-                        checked={state}
-                        onUpdate={(val) => {
-                            setNotification(val);
-                        }}
-                    />
+                />
 
-                    <Text style={{marginLeft: 8}} variant="subheader-1">
-                        Получать уведомления РНП
-                    </Text>
-                </motion.div>
-                <motion.div
-                    style={{position: 'absolute'}}
-                    animate={{opacity: state === undefined ? 1 : 0}}
-                >
-                    <Loader size="s" />
-                </motion.div>
-            </div>
-        </Tooltip>
+                <Text style={{marginLeft: 8}} variant="subheader-1">
+                    Получать уведомления РНП
+                </Text>
+                <div style={{marginLeft: '8px'}}>
+                    <HelpPopover content="РНП (рука на пульсе) - позволяет отслеживать каждый час..."></HelpPopover>
+                </div>
+            </motion.div>
+            <motion.div
+                style={{position: 'absolute'}}
+                animate={{opacity: state === undefined ? 1 : 0}}
+            >
+                <Loader size="s" />
+            </motion.div>
+        </div>
+        // </Tooltip>
     );
 };
