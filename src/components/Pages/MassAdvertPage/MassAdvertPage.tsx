2d819105459412b1ad6dadc1f34d6aa8229b3923
Ledatu <zavoronok.danila@gmail.com>
2025-03-07 04:24:46 +0300

    fix: no drr coloring if cpo

--- src/components/Pages/MassAdvertPage/MassAdvertPage.tsx
+++ src/components/Pages/MassAdvertPage/MassAdvertPage.tsx
@@ -2195,7 +2195,7 @@ export const MassAdvertPage = () => {
                         if (![9, 11].includes(advert?.status)) continue;
                         const drrAI = doc?.advertsAutoBidsRules[selectValue[0]]?.[advert?.advertId];
                         const {desiredDRR, useManualMaxCpm, autoBidsMode} = drrAI ?? {};
-                        if (useManualMaxCpm && !['drr'].includes(autoBidsMode)) continue;
+                        if (useManualMaxCpm || ['cpo'].includes(autoBidsMode)) continue;
                         if (desiredDRR > minDrr) minDrr = desiredDRR;
                     }
                     return minDrr;
